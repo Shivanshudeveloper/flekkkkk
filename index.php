3b880b5a4b212cf047db4cf32333b15cd51e1dff
Shivanshu Gupta <40379768+Shivanshudeveloper@users.noreply.github.com>
2020-01-03 10:02:36 +0530

    heyyy

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>hello</title>
+  </head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-155292410-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-155292410-1');
+</script>
+
+  <body>
+    <p>hello captain</p>
+  </body>
+</html>
